4521ba402e6ccc3fd0d772343b9d054cdfdc9729
IOLANDA LOPES <iolanda.lopes@convenia.com.br>
2022-07-11 09:45:19 -0300

    Criando template para email de boas vindas

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -6,6 +6,20 @@
 </head>
 
 <body>
-    <h1>Welcome to Convenia, {{$employee->name}}</h1>
+    <main margin= 0 auto, display="flex", flex-direction="column">
+        <div  width="150px", padding="5px 5px">
+                <img src="https://s3.amazonaws.com//beta-img.b2bstack.net/uploads/production/product/product_image/389/convenia.jpg" width="150px", padding="5px 5px">
+        </div>
+
+        <div class="w3-panel w3-border">
+            <h2>Bem-vinde à Convenia, {{$employee->name}}!</h2>
+            <p> Muito mais do que apenas desejar boas-vindas,
+            <i>queremos que você sinta esse abraço de chegada</i>
+            . Você vai ser muito feliz aqui!</p>
+
+        </div>
+
+    </main>
+
 </body>
 </html>
